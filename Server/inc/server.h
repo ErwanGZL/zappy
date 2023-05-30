407cc198485a29e931410f86fe9a342a4d2bba48
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-05-30 13:15:38 +0200

    cli arguments parsing

---
+++ Server/inc/server.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** B-YEP-400-MAR-4-1-zappy-erwan.gonzales
+** File description:
+** server
+*/
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/select.h>
