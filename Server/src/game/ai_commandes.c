7400b30f9af1f7b9daecafe89a765f752d30913d
theo <theo.liennard@epitech.eu>
2023-06-07 15:56:08 +0200

    done

--- Server/src/game/ai_commandes.c
+++ Server/src/game/ai_commandes.c
@@ -146,11 +146,25 @@ int check_players(game_t *game, player_t *player, int players_needed, int level_
     return 1;
 }
 
-void resolve_incantation(game_t *game, player_t *player, int nbr_to_lvl_up, int lvl)
+char *resolve_incantation(game_t *game, player_t *player)
 {
+    int lvl = player->entity->level;
+    static const int for_level[7][8] = {
+    //nb player, level_needed, linemate, deraumere, sibur, mendiane, phiras, thystame
+        { 1, 1, 1, 0, 0, 0, 0, 0 },
+        { 2, 2, 1, 1, 1, 0, 0, 0 },
+        { 2, 3, 2, 0, 1, 0, 2, 0 },
+        { 4, 4, 1, 1, 2, 0, 1, 0 },
+        { 4, 5, 1, 2, 1, 3, 0, 0 },
+        { 6, 6, 1, 2, 3, 0, 1, 0 },
+        { 6, 7, 2, 2, 2, 2, 2, 1 }
+    };
+    for (int i = 0; i < 6; i++) {
+        game->map->tiles[player->entity->pos.y][player->entity->pos.x].ressources[i + 1] -= for_level[lvl][i + 2];
+    }
     player->entity->level++;
     int leveled_up = 1;
-    for (list_t ptr = game->players; ptr != NULL && leveled_up <= nbr_to_lvl_up ; ptr = ptr->next) {
+    for (list_t ptr = game->players; ptr != NULL && leveled_up <= for_level[lvl][1] ; ptr = ptr->next) {
         player_t *player2 = ptr->value;
         if (player2->entity->pos.x == player->entity->pos.x && player2->entity->pos.y == player->entity->pos.y) {
             if (player2->entity->level == lvl) {
@@ -159,9 +173,13 @@ void resolve_incantation(game_t *game, player_t *player, int nbr_to_lvl_up, int 
             }
         }
     }
+    char *str = calloc(1, sizeof(char) * 100);
+    memset(str, 0, 100);
+    sprintf(str, "Current level: %d\n", player->entity->level);
+    return str;
 }
 
-int incantate(game_t *game, player_t *player)
+int verif_incantation(game_t *game, player_t *player)
 {
     int lvl = player->entity->level;
     static const int for_level[7][8] = {
@@ -180,9 +198,5 @@ int incantate(game_t *game, player_t *player)
         if (game->map->tiles[player->entity->pos.y][player->entity->pos.x].ressources[i + 1] < for_level[lvl][i + 2])
             return 1;
     }
-    for (int i = 0; i < 6; i++) {
-        game->map->tiles[player->entity->pos.y][player->entity->pos.x].ressources[i + 1] -= for_level[lvl][i + 2];
-    }
-    resolve_incantation(game, player, for_level[lvl][0], lvl);
     return 0;
 }
