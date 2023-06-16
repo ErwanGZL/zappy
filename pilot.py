06f96b102e36e977a7b52bcb1b91aef3fd147cf0
Erwan Gonzales <erwan.gonzales@epitech.eu>
2023-06-16 20:28:37 +0200

    added too much things

--- pilot.py
+++ pilot.py
@@ -8,45 +8,36 @@ import curses.ascii
 import time
 import re
 
+
 class Tile:
     def __init__(self, content: list) -> None:
-        self.food = 0
-        self.linemate = 0
-        self.deraumere = 0
-        self.sibur = 0
-        self.mendiane = 0
-        self.phiras = 0
-        self.thystame = 0
-        self.players = 0
+        self.data = {
+            "food": 0,
+            "linemate": 0,
+            "deraumere": 0,
+            "sibur": 0,
+            "mendiane": 0,
+            "phiras": 0,
+            "thystame": 0,
+            "player": 0,
+            "egg": 0,
+        }
 
         for item in content:
-            if item == "food":
-                self.food += 1
-            elif item == "linemate":
-                self.linemate += 1
-            elif item == "deraumere":
-                self.deraumere += 1
-            elif item == "sibur":
-                self.sibur += 1
-            elif item == "mendiane":
-                self.mendiane += 1
-            elif item == "phiras":
-                self.phiras += 1
-            elif item == "thystame":
-                self.thystame += 1
-            elif item == "player":
-                self.players += 1
+            if item in self.data:
+                self.data[item] += 1
 
     def empty(self) -> bool:
         return (
-            self.food == 0
-            and self.linemate == 0
-            and self.deraumere == 0
-            and self.sibur == 0
-            and self.mendiane == 0
-            and self.phiras == 0
-            and self.thystame == 0
-            and self.players == 0
+            self.data["food"] == 0
+            and self.data["linemate"] == 0
+            and self.data["deraumere"] == 0
+            and self.data["sibur"] == 0
+            and self.data["mendiane"] == 0
+            and self.data["phiras"] == 0
+            and self.data["thystame"] == 0
+            and self.data["player"] == 0
+            and self.data["egg"] == 0
         )
 
     def __repr__(self) -> str:
@@ -57,33 +48,25 @@ class Tile:
 
 class Look:
     def __init__(self, content: str) -> None:
-        self.tiles = []
+        self.tiles: list[Tile] = []
         for tile in content[1:-1].split(","):
             self.tiles.append(Tile(tile.split(" ")))
 
-    def lv4(self) -> str:
-        if len(self.tiles) == 16:
-            return f"{self.tiles[9]}{self.tiles[10]}{self.tiles[11]}{self.tiles[12]}{self.tiles[13]}{self.tiles[14]}{self.tiles[15]}"
-        else:
-            return f"       "
+    def get_lvl(self, lvl) -> str:
+        index = (lvl - 1) ** 2
+        if index < 0 or index >= len(self.tiles):
+            return ""
+        s = ""
+        for i in range((lvl * 2) - 1):
+            s +=  repr(self.tiles[index + i])
+        return s
 
-    def lv3(self) -> str:
-        if len(self.tiles) >= 9:
-            return f" {self.tiles[4]}{self.tiles[5]}{self.tiles[6]}{self.tiles[7]}{self.tiles[8]} "
-        else:
-            return f"       "
-
-    def lv2(self) -> str:
-        if len(self.tiles) >= 4:
-            return f"  {self.tiles[1]}{self.tiles[2]}{self.tiles[3]}  "
-        else:
-            return f"       "
-
-    def lv1(self) -> str:
+    def get_current_tile(self) -> Tile:
         if len(self.tiles) >= 1:
-            return f"   {self.tiles[0]}   "
+            return self.tiles[0]
         else:
-            return f"       "
+            return Tile([])
+
 
 class ManualAI:
     def __init__(self, hostname: str, port: int) -> None:
@@ -99,6 +82,7 @@ class ManualAI:
         curses.init_pair(1, curses.COLOR_RED, -1)
         curses.init_pair(2, curses.COLOR_GREEN, -1)
         curses.init_pair(3, curses.COLOR_YELLOW, -1)
+        curses.init_pair(4, -1, curses.COLOR_WHITE)
 
         # AI Data
         self.team_name = "MANUAL"
@@ -226,8 +210,11 @@ class ManualAI:
 
         last_key = 0
         msg = ''
-        broadcasting = False
         look = Look("")
+        broadcasting = False
+        picking = False
+        dropping = False
+        auto = False
 
         inventory = {
             "food": 0,
@@ -240,6 +227,7 @@ class ManualAI:
         }
 
         while self.connected:
+            current_tile = look.get_current_tile()
             c = self.scr.getch()
             if c != -1:
                 last_key = c
@@ -262,38 +250,104 @@ class ManualAI:
             self.scr.addstr(4, 0, f"map size: {self.map_x}x {self.map_y}y")
             self.scr.addstr(
                 5, 0, f"messages in buffer: {self.get_nb_messages()}")
+            self.scr.addstr(6, 0, f"auto mode:")
+            if auto:
+                self.scr.addstr(6, 11, f"yes", curses.color_pair(2))
+            else:
+                self.scr.addstr(6, 11, f"no", curses.color_pair(1))
             self.scr.addstr(curses.LINES - 5, 0,
-                            str(self.buffer.split()).center(curses.COLS, ' '))
-            self.scr.addstr(2, curses.COLS - 10, look.lv4())
-            self.scr.addstr(3, curses.COLS - 10, look.lv3())
-            self.scr.addstr(4, curses.COLS - 10, look.lv2())
-            self.scr.addstr(5, curses.COLS - 10, look.lv1())
+                            str(self.buffer.split('\n'))[:-1].center(curses.COLS, ' '))
+            self.scr.addstr(9, 10, "Inventory")
+            self.scr.addstr(9, 20, "At reach")
             self.scr.addstr(10, 0, "food")
             self.scr.addstr(10, 10, str(inventory["food"]))
+            self.scr.addstr(10, 20, str(current_tile.data["food"]))
             self.scr.addstr(11, 0, "linemate")
             self.scr.addstr(11, 10, str(inventory["linemate"]))
+            self.scr.addstr(11, 20, str(current_tile.data["linemate"]))
             self.scr.addstr(12, 0, "deraumere")
             self.scr.addstr(12, 10, str(inventory["deraumere"]))
+            self.scr.addstr(12, 20, str(current_tile.data["deraumere"]))
             self.scr.addstr(13, 0, "sibur")
             self.scr.addstr(13, 10, str(inventory["sibur"]))
+            self.scr.addstr(13, 20, str(current_tile.data["sibur"]))
             self.scr.addstr(14, 0, "mendiane")
             self.scr.addstr(14, 10, str(inventory["mendiane"]))
+            self.scr.addstr(14, 20, str(current_tile.data["mendiane"]))
             self.scr.addstr(15, 0, "phiras")
             self.scr.addstr(15, 10, str(inventory["phiras"]))
+            self.scr.addstr(15, 20, str(current_tile.data["phiras"]))
             self.scr.addstr(16, 0, "thystame")
+            self.scr.addstr(16, 10, str(inventory["thystame"]))
+            self.scr.addstr(16, 20, str(current_tile.data["thystame"]))
+            self.scr.addstr(17, 0, "player")
+            self.scr.addstr(17, 20, str(current_tile.data["player"]))
+            self.scr.addstr(18, 0, "egg")
+            self.scr.addstr(18, 20, str(current_tile.data["egg"]))
+
+            self.scr.addstr(9  - 5, curses.COLS - 27, "Commands :")
+            self.scr.addstr(10 - 5, curses.COLS - 27, "<ENTER>  Broadcast")
+            self.scr.addstr(11 - 5, curses.COLS - 27, "<P>      Pick")
+            self.scr.addstr(12 - 5, curses.COLS - 27, "<D>/<S>  Set")
+            self.scr.addstr(13 - 5, curses.COLS - 27, "<!>      Incantation")
+            self.scr.addstr(14 - 5, curses.COLS - 27, "<F>      Fork")
+            self.scr.addstr(15 - 5, curses.COLS - 27, "<E>      Eject")
+            self.scr.addstr(16 - 5, curses.COLS - 27, "<SPACE>  Look")
+            self.scr.addstr(17 - 5, curses.COLS - 27, "<C>      Connect_nbr")
+            self.scr.addstr(18 - 5, curses.COLS - 27, "<ESC>    Quit")
+            self.scr.addstr(19 - 5, curses.COLS - 27, "<A>      Auto mode")
+            self.scr.addstr(20 - 5, curses.COLS - 27, "<+>      Consume response")
+            self.scr.addstr(21 - 5, curses.COLS - 27, "<I>      Inventory")
+            self.scr.addstr(22 - 5, curses.COLS - 27, "<UP>     Move forward")
+            self.scr.addstr(23 - 5, curses.COLS - 27, "<DOWN>   Move backward")
+            self.scr.addstr(24 - 5, curses.COLS - 27, "<LEFT>   Turn left")
 
             center_y = curses.LINES // 2
             center_x = curses.COLS // 2
-
+            if center_y <= 18:
+                center_y = 19
             self.scr.addstr(center_y + 2, 0, ('"' + msg +
                             '"').center(curses.COLS, ' '))
 
-            if broadcasting:
-                if c == curses.ascii.NL:
-                    self.send_message(f"Broadcast {msg}")
-                    broadcasting = False
-                    msg = ''
+            self.scr.addstr(0, center_x - 15, "BROADCASTING")
+            self.scr.addstr(0, center_x, "PICKING")
+            self.scr.addstr(0, center_x + 9, "DROPPING")
 
+            self.scr.addstr(center_y - 5, center_x - 1, look.get_lvl(1))
+            self.scr.addstr(center_y - 6, center_x - 2, look.get_lvl(2))
+            self.scr.addstr(center_y - 7, center_x - 3, look.get_lvl(3))
+            self.scr.addstr(center_y - 8, center_x - 4, look.get_lvl(4))
+            self.scr.addstr(center_y - 9, center_x - 5, look.get_lvl(5))
+            self.scr.addstr(center_y - 10, center_x - 6, look.get_lvl(6))
+            self.scr.addstr(center_y - 11, center_x - 7, look.get_lvl(7))
+            self.scr.addstr(center_y - 2, center_x - 8, look.get_lvl(8))
+
+            if broadcasting:
+                self.scr.addstr(0, center_x - 15, "BROADCASTING", curses.color_pair(4))
+            if picking:
+                self.scr.addstr(0, center_x, "PICKING", curses.color_pair(4))
+            if dropping:
+                self.scr.addstr(0, center_x + 9, "DROPPING", curses.color_pair(4))
+
+
+
+            if broadcasting or picking or dropping:
+                if c == curses.ascii.NL:
+                    if broadcasting:
+                        self.send_message(f"Broadcast {msg}")
+                        broadcasting = False
+                    elif picking:
+                        self.send_message(f"Take {msg}")
+                        picking = False
+                    elif dropping:
+                        self.send_message(f"Set {msg}")
+                        dropping = False
+                    msg = ''
+                elif c == curses.ascii.ESC:
+                    broadcasting = False
+                    picking = False
+                    dropping = False
+                    msg = ''
                 elif c == curses.KEY_BACKSPACE:
                     msg = msg[:-1]
                 elif c != -1:
@@ -313,9 +367,9 @@ class ManualAI:
                     self.send_message("Look")
                 elif c == curses.ascii.NL:
                     broadcasting = True
-                elif c == ord('+'):
+                elif c == ord('+') or auto:
                     m = self.get_next_message()
-                    if m[0] == '[' and m[-1] == ']':
+                    if m and m[0] == '[' and m[-1] == ']':
                         if re.search(r"\w+\s+\d+", m):
                             for s in m[1:-1].split(","):
                                 k, v = s.split()
@@ -327,11 +381,17 @@ class ManualAI:
                 elif c == ord('e'):
                     self.send_message("Eject")
                 elif c == ord('t'):
-                    self.send_message("Take")
+                    picking = True
                 elif c == ord('!'):
                     self.send_message("Incantation")
                 elif c == ord('c'):
                     self.send_message("Connect_nbr")
+                elif c == ord('s'):
+                    dropping = True
+                elif c == ord('d'):
+                    dropping = True
+                elif c == ord('a'):
+                    auto = not auto
 
             if last_key == curses.ascii.ESC:
                 self.scr.addstr(
